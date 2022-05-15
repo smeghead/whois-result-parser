f93452a42bb8648b241810a4958ea2ac990af944
smeghead <smeghead7@gmail.com>
2022-05-16 00:37:29 +0900

    add jp.

--- src/whois-result-parser.js
+++ src/whois-result-parser.js
@@ -1,18 +1,74 @@
+const dayjs = require('dayjs');
+
 class WhoisResultParser {
-    constructor(result) {
+    constructor(domainName, result) {
+        if ( ! domainName) {
+            throw new Exception('ERROR: domainName is required.');
+        }
+        this.domainName = domainName
+        if ( ! result) {
+            throw new Exception('ERROR: result is required.');
+        }
         this.result = result
     }
 
     parse() {
-        return {
-            domainName: 'GOOGLE.COM',
-            "updatedDate": "",
-            "creationDate": "1997-09-15T00:00:00-0700",
-            "expirationDate": "2020-09-13T21:00:00-0700"
-        };
+        console.log(this.result);
+        const lines = this.result.split(/[\r\n]+/);
+
+        const rootDomain = this.domainName.replace(/^.*\.([^\.]*)$/, '$1');
+        let rule = defaultRule;
+        console.log(rootDomain);
+        if (rootDomain in rules) {
+            rule = rules[rootDomain];
+        }
+        console.log(rule);
+        const whois = {};
+        for (const key in rule) {
+            whois[key] = searchPropertyValue(lines, key, rule[key]);
+        }
+        return whois;
     }
 }
 
+const defaultRule = {
+    'domainName': /^Domain Name: *([^\s]+)/,
+    'registrar': /^Registrar: *(.+)/,
+    'updatedDate': /^Updated Date: *(.+)/,
+    'creationDate': /^Creat(?:ed|ion) Date: *(.+)/,
+    'expirationDate': /Expir\w+ Date: *(.+)/,
+};
+const rules = {
+    jp: {
+        'domainName': /^\[Domain Name\]\s+([^\s]+)/,
+        'registrar': /^\[登録年月日\]\s+(.+)$/,
+        'updatedDate': /^\[最終更新\]\s+(.+)$/,
+        'creationDate': /^\[登録年月日\]\s+(.+)$/,
+        'expirationDate': /^\[有効期限\]\s+(.+)$/,
+    },
+};
+
+const searchPropertyValue = (lines, key, regex) => {
+    const values = lines.map(line => {
+        const matches = line.match(regex);
+        if ( ! matches) {
+            return null;
+        }
+        let val = matches[1];
+        if (key.endsWith('Date')) {
+            if (val) {
+                const d = dayjs(val);
+                val = d.format();
+            }
+        }
+        return val;
+    }).filter(val => val);
+    if (values.length === 0) {
+        return null;
+    }
+    console.log(values);
+    return values[0];
+};
 
 module.exports = WhoisResultParser;
 // vim: set expandtab ts=4 sts=4 sw=4:
--- test/whois-result-parser.test.js
+++ test/whois-result-parser.test.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const WhoisResultParser = require('../src/whois-result-parser');
 
 test('initialize', () => {
-    const raw = fs.readFileSync('./test/data/google.com');
-    const parser = new WhoisResultParser(raw);
+    const raw = fs.readFileSync('./test/data/google.com.txt');
+    const parser = new WhoisResultParser('google.com', raw);
     expect(parser).not.toBe(null);
 });
 
@@ -13,10 +13,12 @@ const checkAnswer = (actual, expected) => {
     expect(actual.creationDate).toBe(expected.creationDate);
     expect(actual.expirationDate).toBe(expected.expirationDate);
 };
-test('initialize', () => {
-    const raw = fs.readFileSync('./test/data/google.com');
-    const answer = fs.readFileSync('./test/data/google.com.json');
-    const parser = new WhoisResultParser(raw);
-    checkAnswer(parser.parse(), JSON.parse(answer));
+fs.readdirSync('./test/data').filter(name => name.endsWith('.txt')).forEach(file => {
+    test(`test all domains ${file}`, () => {
+            const raw = fs.readFileSync(`./test/data/${file}`, 'utf-8');
+            const answer = fs.readFileSync(`./test/data/${file.replace(/\.txt$/, '.json')}`, 'utf-8');
+            const parser = new WhoisResultParser(`${file.replace(/\.txt$/, '')}`, raw);
+            checkAnswer(parser.parse(), JSON.parse(answer));
+    });
 });
 // vim: set expandtab ts=4 sts=4 sw=4:
